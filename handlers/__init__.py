a9ca67ec19dd2763372f0f1cce3fc456087c1da7
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-03-26 08:10:49 +0300

    Зарегистрировал router для просмотра заказов

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Dispatcher
 from handlers.start import router as start_router
 from handlers.ready_cake import router as ready_cake_router
+from handlers.customer_orders import router as customer_orders_router
 
 
 def register_routes(dp: Dispatcher):
     dp.include_router(start_router)
     dp.include_router(ready_cake_router)
+    dp.include_router(customer_orders_router)
